f8a1a604bfedc7fde82a04b8d957b48e3fe4d377
DJergensen13 <157402971+DJergensen13@users.noreply.github.com>
2024-01-31 12:29:28 -0700

    double print issue

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ int winFlag = 0;
 Console.WriteLine("Welcome to Tic-Tac-Toe!");
 
 // Game loop
-while (Supporting.Winner(winFlag) == 0)
+while (Supporting.Winner(winFlag, currentPlayer, board) == 0)
 {
     // Print the current board
     Console.WriteLine();
@@ -37,7 +37,6 @@ while (Supporting.Winner(winFlag) == 0)
     {
         Console.WriteLine("Invalid move. Try again.");
     }
-    Supporting.Winner(winFlag, currentPlayer, board);
 }
 
 // Game over, print the final board
