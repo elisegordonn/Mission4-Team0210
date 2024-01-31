16b354b9a5ebe1b5944a4303c8d9974e87da79d3
taitertot18 <lilysharontait@icloud.com>
2024-01-31 12:02:04 -0700

    fixed board again

--- Supporting.cs
+++ Supporting.cs
@@ -18,13 +18,13 @@ namespace Mission4_Team0210
 
             //tic tac toe board
             Console.WriteLine("     |     |     ");
-            Console.WriteLine("  {0}  |   {1}  |  {2}  ", board[1], board[2], board[3]);
+            Console.WriteLine("  {0}  |   {1}  |  {2} ", board[1], board[2], board[3]);
             Console.WriteLine("_____|_____|_____");
             Console.WriteLine("     |     |     ");
-            Console.WriteLine("  {0}  |   {1}  |  {2}  ", board[4], board[5], board[6]);
+            Console.WriteLine("  {0}  |   {1}  |  {2} ", board[4], board[5], board[6]);
             Console.WriteLine("_____|_____|_____");
             Console.WriteLine("     |     |     ");
-            Console.WriteLine("  {0}  |   {1}  |  {2}  ", board[7], board[8], board[9]);
+            Console.WriteLine("  {0}  |   {1}  |  {2} ", board[7], board[8], board[9]);
             Console.WriteLine("     |     |     ");
         }
 
