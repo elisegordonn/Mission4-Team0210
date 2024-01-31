e9a1c1a6752dccd719964b64e171a9d59059e635
taitertot18 <lilysharontait@icloud.com>
2024-01-31 12:09:10 -0700

    added a comment

--- Supporting.cs
+++ Supporting.cs
@@ -66,6 +66,8 @@ namespace Mission4_Team0210
             {
                 winFlag = 1;
             }
+
+            //if there is a draw
             else if (board[1] != '1' && board[2] != '2' && board[3] != '3' && board[4] != '4'
                 && board[5] != '5' && board[6] != '6' && board[7] != '7' && board[8] != '8' && board[9] != '9')
             {
