5e723c6be8bbf709405331839b8f7a4fa74f576c
elisegordonn <elisegordonn@gmail.com>
2024-02-01 10:55:10 -0700

    final with comments

--- Program.cs
+++ Program.cs
@@ -1,22 +1,26 @@
-﻿using Mission4_Team0210;
+﻿//Mission 4, Section 2, Team 10
+// Daxton Jergensen, Elise Gordon, Lily Tait, Rai Martin
+using Mission4_Team0210;
 using System.Net;
+
 // Create an instance of the supporting class
 Supporting Supporting = new Supporting();
 
-
+//initialize variables and board array
 int currentPlayer = 1;
 char[] board = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 int winFlag = 0;
 
 Console.WriteLine("Welcome to Tic-Tac-Toe!");
 
-// Game loop
+// Game loop runs while winFlag = 0
 while (Supporting.Winner(winFlag, currentPlayer, board) == 0)
 {
     // Print the current board
     Console.WriteLine();
     Supporting.Board(board);
     Console.WriteLine();
+
     // Get the current player's move
     Console.WriteLine($"Player {currentPlayer}'s turn.");
     Console.Write("Enter space (1-9): ");
@@ -31,6 +35,8 @@ while (Supporting.Winner(winFlag, currentPlayer, board) == 0)
         {
             board[numboard] = 'O';
         }
+
+        //switch player number between 1 and 2
         currentPlayer = (currentPlayer == 1) ? 2 : 1;
     }
     else
