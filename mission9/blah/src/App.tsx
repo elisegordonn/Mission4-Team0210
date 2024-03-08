bae133db406da85296b9e471e1b24dff5ecc83dd
uriah_ah <109769619+ukaqm@users.noreply.github.com>
2024-03-08 12:35:46 -0700

    hi

--- mission9/blah/src/App.tsx
+++ mission9/blah/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn to do stuff
+          Learn to do
         </a>
       </header>
     </div>
